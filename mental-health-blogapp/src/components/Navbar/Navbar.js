95dad2fc24702f68fe6d3df1d521225a5bde327e
Justin <jbcool02@yahoo.com>
2025-01-08 16:41:12 -0600

    Structure setup

---
+++ mental-health-blogapp/src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Navbar.css';
+
+function Navbar()
+{
+    return (
+        <nav className="navbar">
+            <h1>My mental health blog site</h1>
+        </nav>
+    );
+}
+
+export default Navbar;
