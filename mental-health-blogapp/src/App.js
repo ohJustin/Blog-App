ea87d19328995946c8e083684777053d966c9534
Justin <jbcool02@yahoo.com>
2025-01-09 13:09:40 -0600

    Centered & resized navbar

--- mental-health-blogapp/src/App.js
+++ mental-health-blogapp/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import Navbar from '/components/Navbar/Navbar';
 import MuiNavbar from './components/Navbar/MuiNavbar.tsx';
 
 import './App.css';
@@ -7,12 +6,10 @@ import './App.css';
 function App() {
   return (
     <MuiNavbar position='static'>
-    <div className="App"> 
-        <div className="main-content">
+    <div className="main-content">
 
           {/* Main Content here... */}
-
-        </div>
+          
     </div>
     </MuiNavbar>
   );
--- mental-health-blogapp/src/components/Navbar/MuiNavbar.tsx
+++ mental-health-blogapp/src/components/Navbar/MuiNavbar.tsx
@@ -1,25 +1,34 @@
 import { AppBar, Toolbar, IconButton, Typography, Stack, Button } from "@mui/material";
 import HealthIcon from '../../assets/mental-health.png';
+import './Navbar.css';
 import React from "react";
 
 const MuiNavbar = () => {
     return (
-        <AppBar position='static'>
-            <Toolbar>
-                <IconButton size='small' edge='start' color='inherit' aria-label='logo'>
-                    <img src={HealthIcon} alt="Health Icon" />
-                </IconButton>
-                <Typography variant='h6' component='div'>
-                    BRAINAPP
-                </Typography>
-                <Stack direction='row' spacing={2}>
-                    <Button color='inherit'> TestBtn </Button>
-                    <Button color='inherit'> TestBtn2 </Button>
-                    <Button color='inherit'> TestBtn3 </Button>
-                </Stack>
-            </Toolbar>
-        </AppBar>
-    )
-};
+      <AppBar position="static" classes={ { root: 'custom-navbar' } }>
 
-export default MuiNavbar;
+        <Toolbar classes={{ root: 'custom-toolbar' }} >
+
+          <IconButton size="small" edge="start" color="inherit" aria-label="logo" className="custom-logo-container">
+            <img src={HealthIcon} alt="Health Icon" className="custom-logo"/>
+            <Typography  variant="h6" component="div" className="custom-app-name">
+              BRAINAPP
+            </Typography>
+          </IconButton>
+
+          <Stack direction="row" spacing={2} className="custom-nav-buttons">
+            {['TestBtn', 'TestBtn2', 'TestBtn3'].map((btnText) => (
+              <Button key={btnText} color="inherit" className="custom-nav-btn">
+                {btnText}
+              </Button>
+            ))}
+          </Stack>
+
+        </Toolbar>
+        
+      </AppBar>
+    );
+  };
+  
+  export default MuiNavbar;
+  
