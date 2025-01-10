f8147abea7c188275f708c2e9f63fa7e4d97d69d
Justin <jbcool02@yahoo.com>
2025-01-10 14:17:47 -0600

    Work done for CREATE post modal

--- mental-health-blogapp/src/pages/CreatePost.js
+++ mental-health-blogapp/src/pages/CreatePost.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import DefaultModal from '../components/Modal/Modal';
 
 function CreatePost() {
+  <DefaultModal></DefaultModal>
   return <div>Create a new post here!</div>;
 }
 
